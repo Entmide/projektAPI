20a7dbafb0418701570266ca0560530d12ddc6c8
Entmide <88313951+Entmide@users.noreply.github.com>
2022-11-30 00:39:46 +0100

    Add files via upload

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,30 @@
-
 from locust import HttpUser, task
 import random
+import requests
+import json
 
 numbers = [1, 0, 3456, 311313, 455356356, 52525, 66547, 77577, 90800897]
 
+
 class WebsiteUser(HttpUser):
+
+    token = ''
+
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        with self.client.post("token", {
+            "username": "your_username",
+            "password": "your_password"
+        }) as response:
+            self.token = response.json().get('token')
 
     @task
     def getdate(self):
-        self.client.get(url='date/', headers={"Authorization": "token"})
+        self.client.get(url='date', headers={"Authorization": self.token})
 
-        #headers = {"username": "user1", "password": "123456qwerty"
+    # headers = {"username": "user1", "password": "123456qwerty"
 
     @task
     def prime(self):
